a95867274a0fd522f66354b619690cb3025b9f69
efugikawa <42985772+efugikawa@users.noreply.github.com>
2022-03-08 13:49:07 -0700

    add logging and argparse

--- precip_crime_study.py
+++ precip_crime_study.py
@@ -2,13 +2,36 @@
 """
 Created on Wed Mar  2 21:06:38 2022
 
-@author: bkmcc
+@author: bkmcc, ebf
 """
 import FP_precip_load as rain
 import FP_crime_load as crime
 import FP_CO_county_load as cnty
 import pandas as pd
 import geopandas as gpd
+import logging
+import argparse
+
+
+def log_config():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)  # i.e. everything
+
+    fh = logging.FileHandler("precip_crime_logs.log", 'w')
+    fh.setLevel(logging.DEBUG)  # everything prints to log file
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.INFO)  # only INFO and greater print to console
+    logger.addHandler(sh)
+
+
+def parser_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year", type=int, choices=[1999, 2000, 2001, 2002, 2003, 2004, 2005,
+                        2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+                        help="four digit year from 1999 to 2015")
+    return parser.parse_args()
 
 
 def gpd_County(rain_DF, county_DF):
@@ -22,9 +45,10 @@ def gpd_County(rain_DF, county_DF):
 
 
 def main():
-
-    # replace tempYear with args input later
-    tempYear = 2012
+    log_config()
+    args = parser_config()
+    
+    tempYear = args.year
     # load precipitation data for selected year
     df_Precip = rain.load_CO_precip_data(tempYear)
     # load crime data for selected year
